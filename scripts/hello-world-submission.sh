c7ceb0a0a0ab3525ab4afd6516277b69dd4a6637
ejh243 <e.j.hannon@ex.ac.uk>
2022-12-08 12:33:37 +0000

    change to training queue

--- scripts/hello-world-submission.sh
+++ scripts/hello-world-submission.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #SBATCH --export=ALL # export all environment variables to the batch job.
-#SBATCH -p sq # submit to the serial queue
+#SBATCH -p training # submit to the serial queue
 #SBATCH --time=00:10:00 # Maximum wall time for the job.
 #SBATCH -A Research_Project-HPC-Training # research project to submit under. 
 #SBATCH --nodes=1 # specify number of nodes.
-#SBATCH --ntasks-per-node=16 # specify number of processors per node
+#SBATCH --ntasks-per-node=1 # specify number of processors per node
 #SBATCH --mail-type=END # send email at job completion 
 #SBATCH --output=hello-world.o
 #SBATCH --error=hello-world.e
